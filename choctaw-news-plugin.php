e1b0917b8804c095b2f42758f69df2358e3aed55
KJ Roelke <kj.roelke@gmail.com>
2024-04-18 09:59:55 -0500

    update requires plugin list

--- choctaw-news-plugin.php
+++ choctaw-news-plugin.php
@@ -11,7 +11,7 @@
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  * Requires PHP: 8.1
  * Requires at least: 6.0
- * Requires Plugin: acf
+ * Requires Plugin: advanced-custom-fields-pro
  *
  * @package ChoctawNation
  * @subpackage News
